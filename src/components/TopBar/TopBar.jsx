b4b554ba4a30bf31058190ba55586da394294ed3
sam <natalsamuel23@gmail.com>
2022-11-29 14:36:35 +0300

    Initial setup for the shopgram admin panel

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,20 @@
+import './TopBar.scss';
+// import {User} from '../index.js';
+
+
+const TopBar = () => {
+  return (
+    <div className='topBar'>
+        <div className="date-user-container flex-row">
+          <div className="date">
+            
+          </div>
+          <div className="user">
+            <img src="" alt="x" />
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default TopBar
